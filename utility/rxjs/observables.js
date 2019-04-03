44196c30c3e800cd58cb6d69e9065047ff59764d
David Shams <david.shams@gmail.com>
2019-04-02 22:21:09 -0500

    * next()   * error()   * complete()

--- utility/rxjs/observables.js
+++ utility/rxjs/observables.js
@@ -1,1 +1,13 @@
 import {Observable, from} from 'rxjs';
+let nums$ = from([1,2,3,4,5,6]);
+/**
+ * Observable:
+ * subscribe()
+ */
+
+ /**
+  * Observer:
+  * next()
+  * error()
+  * complete()
+  */
