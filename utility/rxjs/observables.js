148ffcc363225fa2cc5a0d93824b5aed7be8f8d6
David Shams <david.shams@gmail.com>
2019-04-02 22:26:05 -0500

    * subscrive to an observable passing an observer

--- utility/rxjs/observables.js
+++ utility/rxjs/observables.js
@@ -11,3 +11,12 @@ let nums$ = from([1,2,3,4,5,6]);
   * error()
   * complete()
   */
+ let observer = {
+     next: value => console.log(value),
+     error: err => console.error(`Error: ${err}`),
+     complete: () => console.log('All done!')
+ }
+
+ /**
+  * subscrive to an observable passing an observer
+  */
